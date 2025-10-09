cb774a0c48d7ce0c935d8a3879989712918c1d58
TaraDenniston <taradenniston@gmail.com>
2025-10-09 13:32:00 -0400

    add class names to Pokedex component

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import pokemon from './Pokemon'
 import Pokecard from './Pokecard'
 
 export default function Pokedex(props) {
   return (
     <div>
-      <h2>Pokedex</h2>
-      <div>
+      <h2 className="poketitle">Pokedex</h2>
+      <div className="pokedex">
         {pokemon.map(p => (
           <Pokecard key={p.id} id={p.id} name={p.name} type={p.type} base_experience={p.base_experience}/>
         ))}
