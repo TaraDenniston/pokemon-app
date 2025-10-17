991c92a9930b88be770de54e3eecdcaca0c4d86b
TaraDenniston <taradenniston@gmail.com>
2025-10-17 13:49:12 -0400

    add styling to Pokecard component

--- src/components/Pokecard.jsx
+++ src/components/Pokecard.jsx
@@ -1,10 +1,12 @@
+import '../styles/Pokecard.css'
+
 export default function Pokecard(props) {
   const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`;
   return (
-    <div className="card">
-      <h4 className="card-name">{props.name}</h4>
-      <img className="card-img" src={url} alt={props.name} />
-      <ul className="card-stats">
+    <div className="Pokecard">
+      <h4 className="Pokecard-name">{props.name}</h4>
+      <img className="Pokecard-img" src={url} alt={props.name} />
+      <ul className="Pokecard-stats">
         <li>Type: {props.type}</li>
         <li>EXP: {props.base_experience}</li>
       </ul>
