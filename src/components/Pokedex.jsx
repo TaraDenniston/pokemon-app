764966d886f85445f7fb317aa3f0cfdcda13bb68
TaraDenniston <taradenniston@gmail.com>
2025-10-17 13:25:53 -0400

    add styling for Pokedex component

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,11 +1,12 @@
 import pokemon from './Pokemon'
 import Pokecard from './Pokecard'
+import '../styles/Pokedex.css'
 
 export default function Pokedex(props) {
   return (
-    <div>
-      <h2 className="poketitle">Pokedex</h2>
-      <div className="pokedex">
+    <div className="Pokedex-container">
+      <h2 className="Pokedex-title">Pokedex</h2>
+      <div className="Pokedex-cards">
         {pokemon.map(p => (
           <Pokecard key={p.id} id={p.id} name={p.name} type={p.type} base_experience={p.base_experience}/>
         ))}
